65bab9a1885f10b48288bb5db6a5fcc32fbbef20
momoelabdi <momo.elabdi@bluewin.ch>
2025-06-05 21:26:12 +0200

    init

---
+++ src/test/java/com/env/windCatchers/WindCatchersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.env.windCatchers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindCatchersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
