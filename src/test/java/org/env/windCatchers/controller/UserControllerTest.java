ae1e19d3172eec9249920edb5e23fdf00471a9fd
momoelabdi <momo.elabdi@bluewin.ch>
2025-06-10 22:15:31 +0200

    user api test

--- src/test/java/org/env/windCatchers/controller/UserControllerTest.java
+++ src/test/java/org/env/windCatchers/controller/UserControllerTest.java
@@ -9,13 +9,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-
-import org.env.windCatchers.controllers.UserController;
+import org.env.windCatchers.controllers.UsersController;
 import org.env.windCatchers.model.User;
 import org.env.windCatchers.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -25,7 +27,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
-@WebMvcTest(UserController.class)
+@WebMvcTest(UsersController.class)
 public class UserControllerTest {
 
     @Autowired
@@ -52,6 +54,18 @@ public class UserControllerTest {
         mvc.perform(get("/api/users"))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.size()", is(users.size())));
+    }
+
+    @Test
+    void shouldFindUserById() throws Exception {
+        User user  =  users.get(0);
+        when(repository.findById(ArgumentMatchers.anyLong())).thenReturn(Optional.of(user));
+        mvc.perform(get("/api/users/1"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.name", is(user.getName())))
+            .andExpect(jsonPath("$.email", is(user.getEmail())))
+            .andExpect(jsonPath("$.role", is(user.getRole())))
+            .andExpect(jsonPath("$.phone", is(user.getPhone())));
     }
 
 
