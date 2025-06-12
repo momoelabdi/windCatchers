5a61b90ea33a8dbe2e8970373daf93585899a525
momoelabdi <momo.elabdi@bluewin.ch>
2025-06-12 11:11:43 +0200

    init Schedules api

---
+++ src/main/java/org/env/windCatchers/controllers/SchedulesController.java
@@ -0,0 +1,31 @@
+package org.env.windCatchers.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+import org.env.windCatchers.model.Schedule;
+import org.env.windCatchers.repository.ScheduleRepository;
+
+@RestController
+@RequestMapping("/api/schedules")
+public class SchedulesController {
+
+
+    
+    private final ScheduleRepository scheduleRepository;
+
+    public SchedulesController(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    
+
+    @PostMapping
+    public Schedule create(@Valid @RequestBody Schedule schedule) {
+        return scheduleRepository.save(schedule);
+    }
+}
